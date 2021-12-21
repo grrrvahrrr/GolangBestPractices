7bc0cb46fc48eeb8f47fc04b810b71abac37469a
Sergei <kabernik@gmail.com>
2021-12-21 17:48:52 +0300

    Added Parsing if search parameters

--- process/process.go
+++ process/process.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
@@ -26,11 +27,13 @@ type ProcessAll interface {
 }
 
 type UserRequest struct {
-	RequestBody string
-	ColumnName  []string
-	FileName    string
-	SearchName  []string
-	SearchParam []string
+	RequestBody     string
+	FileName        string
+	ColumnName      []string
+	SearchBody      []string
+	SearchParamName []string
+	SearchParam     []string
+	SearchValue     []string
 }
 
 func (r UserRequest) ReadFile() {
@@ -41,12 +44,11 @@ func (r UserRequest) ReadFile() {
 	defer file.Close()
 
 	csvReader := csv.NewReader(file)
-	counter := 0
-	var index []int
+	var indexCol []int
+	var indexParam []int
+	var counter int
 
-	//var header bool = true
-	for counter < 10 {
-
+	for counter < 15 {
 		rec, err := csvReader.Read()
 		if err == io.EOF {
 			break
@@ -60,20 +62,93 @@ func (r UserRequest) ReadFile() {
 		for _, iv := range r.ColumnName {
 			for j, jv := range rec {
 				if jv == iv {
-					index = append(index, j)
+					indexCol = append(indexCol, j)
 				}
 			}
 		}
-		//if !header {
-		var sliceToPrint []string
-		for _, v := range index {
-			sliceToPrint = append(sliceToPrint, rec[v])
-		}
-		log.Info(sliceToPrint)
-		counter++
-		//}
 
-		//header = false
+		for _, iv := range r.SearchParamName {
+			for j, jv := range rec {
+				if jv == iv {
+					indexParam = append(indexParam, j)
+				}
+			}
+		}
+
+		// log.Info(r.SearchParamName)
+		// log.Info(indexParam)
+
+		var sliceToPrint []string
+		if indexParam == nil {
+			for _, v := range indexCol {
+				sliceToPrint = append(sliceToPrint, rec[v])
+			}
+			log.Info(sliceToPrint)
+		} else {
+			for i, v := range indexParam {
+				switch r.SearchParam[i] {
+				case ">":
+					if len(indexParam) == 1 {
+						recValue, _ := strconv.ParseFloat(rec[v], 64)
+						value, _ := strconv.ParseFloat(r.SearchValue[i], 64)
+						if recValue > value {
+							for _, v := range indexCol {
+								sliceToPrint = append(sliceToPrint, rec[v])
+							}
+							sliceToPrint = append(sliceToPrint, rec[v])
+							log.Info(sliceToPrint)
+						}
+					} else {
+						//log.Info("There is more than 1 param")
+						var recValueSlice []float64
+						for _, v := range indexParam {
+							recValue, _ := strconv.ParseFloat(rec[v], 64)
+							recValueSlice = append(recValueSlice, recValue)
+						}
+						//log.Info(recValueSlice)
+						var valueSlice []float64
+						for i := range r.SearchValue {
+							value, _ := strconv.ParseFloat(r.SearchValue[i], 64)
+							valueSlice = append(valueSlice, value)
+						}
+						//log.Info(valueSlice)
+
+						if sliceToPrint == nil {
+							for _, v := range indexCol {
+								sliceToPrint = append(sliceToPrint, rec[v])
+							}
+							func() {
+								for i := range recValueSlice {
+									for range valueSlice {
+										//log.Info(recValueSlice)
+										//log.Info(valueSlice)
+										if recValueSlice[i] < valueSlice[i] {
+											return
+										}
+									}
+									if len(sliceToPrint) < (len(indexCol) + len(indexParam)) {
+										for _, v := range indexParam {
+											sliceToPrint = append(sliceToPrint, rec[v])
+										}
+									}
+								}
+								log.Info(sliceToPrint)
+							}()
+
+						}
+					}
+
+				}
+
+			}
+
+		}
+
+		//counter++
+		// if sliceToPrint != nil {
+		// 	log.Info(sliceToPrint)
+		// }
+
 	}
 
 }
@@ -104,6 +179,39 @@ func (r *UserRequest) ParseRequest() {
 	for _, v := range bodySlice {
 		if v == "WHERE" {
 			r.FileName = strings.TrimSpace(between(r.RequestBody, "FROM", "WHERE"))
+
+			//log.Info(r.FileName)
+			//Parse Search parameters
+			r.SearchBody = strings.Fields(after(r.RequestBody, "WHERE"))
+			r.SearchParamName = append(r.SearchParamName, r.SearchBody[0])
+			for i, v := range r.SearchBody {
+				if v == "AND" {
+					r.SearchParamName = append(r.SearchParamName, r.SearchBody[i+1])
+				}
+			}
+
+			r.SearchParam = append(r.SearchParam, r.SearchBody[1])
+			for i, v := range r.SearchBody {
+				if v == "AND" {
+					r.SearchParam = append(r.SearchParam, r.SearchBody[i+2])
+				}
+			}
+			//r.SearchParam = r.SearchBody[1]
+			r.SearchValue = append(r.SearchValue, r.SearchBody[2])
+			for i, v := range r.SearchBody {
+				if v == "AND" {
+					r.SearchValue = append(r.SearchValue, r.SearchBody[i+3])
+				}
+			}
+			//r.SearchValue = r.SearchBody[2]
+
+			// log.Info(r.SearchBody)
+			// log.Info(r.SearchParamName)
+			// log.Info(r.SearchParam)
+			// log.Info(r.SearchValue)
+
+			break
+
 		} else {
 			r.FileName = strings.TrimSpace(after(r.RequestBody, "FROM"))
 		}
@@ -141,3 +249,5 @@ func after(value string, a string) string {
 	}
 	return value[adjustedPos:len(value)]
 }
+
+//SELECT SNo, Country/Region FROM /home/deus/Documents/testData/covid_19_data.csv WHERE Confirmed > 100 AND Deaths > 50 AND Recovered > 50
